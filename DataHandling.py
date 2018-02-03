aa7bc3c7b80c8b2d0e2d8843f3c7a3f75b67f128
dparker.g2 <dparker.g2@gmail.com>
2018-02-03 17:09:53 -0500

    update

--- DataHandling.py
+++ DataHandling.py
@@ -2,7 +2,7 @@
 """############################################################################
 ###
 ### DataHandling
-### This file is part of Snippets
+### This file is part of Data-Processing
 ### This file was created by Dr Daniel Parker on Sat Feb  3 17:54:10 2018 
 ###    Twitter: @DrDanParker     GitHub:https://github.com/DrDanParker 
 ###     
@@ -11,7 +11,7 @@
 ### See LICENSE or go to https://tldrlegal.com/license/mit-license for full licence details
 ###
 ### Based on the following:
-###     
+###     general panda docs and online tutorials
 ###     
 ###
 ############################################################################"""
@@ -30,5 +30,17 @@ class Panda_Files:
         filename, file_extension = os.path.splitext(self)
         self.ftype = file_extension     ### file type from extension 
         
-    def filetype(self):
+    def load_file(self):
+        if self.ftype == '.csv' or '.txt' or '.asc':
+          outfile = pd.read_csv(self,sep=delim)
+        elif self.ftype == '.xls' or '.xlsx':
+          outfile = pd.ExcelFile(self)
+        elif self.type == '.json':
+          outfile = pd.read_json(self) 
+        else:
+          print('not recognised file type')
+        return(outfile)
+      
+          
+          
                 
